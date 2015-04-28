5d77182c8fe84d39a1e887d0eeb98d8ab38012fb
DK <danki99@gmail.com>
2015-04-28 12:18:30 +0200

    działa dekodowanie jednego bloku

--- main.c
+++ main.c
@@ -225,6 +225,51 @@ bit* encryptBlock(bit* input){
     out = permute(64, out, 64, permodwrotna);
     return out;
 }
+bit* decryptBlock(bit* input){
+    //pobiera 64 bity i zwraca zaszyfrowany blok
+
+    //initial permutation
+    uint perm[64] = {
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7,
+        56, 48, 40, 32, 24, 16, 8, 0,
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6
+    };
+    uint permodwrotna[64] = {
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25,
+        32, 0, 40, 8, 48, 16, 56, 24
+    };
+    bit* permuted = permute(64, input, 64, perm);
+    bit* l = permuted;
+    bit* r = permuted+32;
+
+    pair state;
+    state.L = l;
+    state.R = r;
+    for(int i=0; i<16; i++){
+        state = runda(15-i, state);
+    }
+
+    bit* out = (bit*)calloc(64, sizeof(bit));
+    for(int i=0; i<32; i++){
+        out[i] = state.R[i];
+    }
+    for(int i=0; i<32; i++){
+        out[i+32] = state.L[i];
+    }
+    out = permute(64, out, 64, permodwrotna);
+    return out;
+}
 
 int main(int argc, char const *argv[]){
     // bit* bity;
@@ -233,12 +278,15 @@ int main(int argc, char const *argv[]){
     // int test = bits2num(64, bity);
     // printf("cokolwiek, %d\n", test);
     // test();
-    bit* bits = hex2bits(16, "0123456789ABCDEF");
+    bit* bits = hex2bits(16, "85E813540F0AB405");
     printf("ZZZ\n");
-    bits = encryptBlock(bits);
-    printbits(hex2bits(16, "85E813540F0AB405"));
+    bits = decryptBlock(bits);
+    printbits(hex2bits(16, "0123456789ABCDEF"));
     printf("\n");
     printbits(bits);
 
     return 0;
 }
+
+//plaintext 0123456789ABCDEF
+//szyfr 85E813540F0AB405
